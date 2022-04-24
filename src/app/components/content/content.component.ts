a4068606e15629cc891d5c1ae178836a3e219d85
Wallace <wallace_wolv@hotmail.com>
2022-04-24 20:41:18 -0300

    feat: Adicionando animação no content e ajustes nos width s da aplicação

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
+
 
 @Component({
   selector: 'fake-bank-content',
@@ -10,6 +12,26 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: [
+        'Fake Banking com juros amigo',
+        'Peça a portabilidade e venha ser Fake Bankinhg',
+        'Inovador',
+        'Digital',
+        'Seguro'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
+
+    const typed = new Typed('.typing-element', options);
   }
 
 }
